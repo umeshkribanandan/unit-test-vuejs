89c893f43c85dca02f5a9f961830b94103b23fca
Umesh Kribanandan <umesh.kribanandan@gmail.com>
2020-04-19 00:01:02 +0530

    unit testing

---
+++ tests/unit/LoginForm.spec.js
@@ -0,0 +1,24 @@
+import LoginForm from '@/components/LoginForm.vue'
+import {
+  mount
+} from '@vue/test-utils'
+
+describe('LoginForm', () => {
+  it('emits an event with a user data payload', () => {
+    const wrapper = mount(LoginForm)
+    // 1. Find text input
+    const input = wrapper.find('input[type="text"]')
+    // 2. Set value for text input
+    input.setValue('Umesh')
+    // 3. Simulate form submission
+    wrapper.trigger('submit')
+    // 4. Assert event has been emitted
+    const formSubmittedCalls = wrapper.emitted('formSubmitted')
+    expect(formSubmittedCalls).toHaveLength(1)
+    // 5. Assert payload is correct
+    const expectedPayload = {
+      name: 'Umesh'
+    }
+    expect(wrapper.emitted('formSubmitted')[0][0]).toMatchObject(expectedPayload)
+  })
+})
---
+++ tests/unit/RandomNumber.spec.js
@@ -0,0 +1,38 @@
+import {
+  mount
+} from '@vue/test-utils'
+import RandomNumber from '@/components/RandomNumber'
+
+describe('RandomNumber', () => {
+  test('By default, randomNumber data value should be 0', () => {
+    const wrapper = mount(RandomNumber)
+    expect(wrapper.html()).toContain('<span>0</span>')
+  })
+
+  test('If button is clicked, randomNumber should be between 1 and 10', async () => {
+    const wrapper = mount(RandomNumber)
+    wrapper.find('button').trigger('click')
+
+    // Wait for any reactivity changes to appear in the DOM
+    await wrapper.vm.$nextTick()
+
+    // This is now a random number instead of 0
+    const randomNumber = parseInt(wrapper.find('span').element.textContent)
+  })
+
+  test('If button is clicked, randomNumber should be between 200 and 300', async () => {
+    const wrapper = mount(RandomNumber, {
+      propsData: {
+        min: 200,
+        max: 300
+      }
+    })
+
+    wrapper.find('button').trigger('click')
+    await wrapper.vm.$nextTick()
+
+    const randomNumber = parseInt(wrapper.find('span').element.textContent)
+    expect(randomNumber).toBeGreaterThanOrEqual(200)
+    expect(randomNumber).toBeLessThanOrEqual(300)
+  })
+})
